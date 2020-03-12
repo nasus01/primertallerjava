059b9bc49e43fc1d571b0313920f68ac3b88e286
christian morales <christianjack01@hotmail.com>
2020-03-11 20:51:30 -0500

    primer taller java

---
+++ src/punto1/ejercicio2.java
@@ -0,0 +1,24 @@
+package punto1;
+import java.util.Scanner;
+public class ejercicio2 {
+
+	
+
+
+	    public static void main(String args[]) {
+
+	        Scanner teclado = new Scanner(System.in);
+
+	        System.out.println("ingrese valor para mostrar la tabla de multiplicar hasta 10");
+
+	        int valor = teclado.nextInt();
+
+	        int i; //for
+
+	        for (i = 1; i <= 10; i++) {
+	            System.out.println(valor + " x " + i + " = " + (valor * i));
+	        }
+	    }
+	    
+	
+}
